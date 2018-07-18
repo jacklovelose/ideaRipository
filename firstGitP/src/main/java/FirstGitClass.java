3fa1f021548027a88c3c6ec7bf93a88356249c4c
sublun <sublun@126.com>
2018-07-18 10:15:39 +0800

    添加测试类

--- firstGitP/src/main/java/FirstGitClass.java
+++ firstGitP/src/main/java/FirstGitClass.java
@@ -2,4 +2,7 @@ public class FirstGitClass {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaa");
     }
+    public void test(){
+        System.out.println("ssssssssssssssssssss");
+    }
 }
